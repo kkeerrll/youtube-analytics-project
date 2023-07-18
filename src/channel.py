a71afa008ec766c2365c07e2b0444423d3c07962
anzelikagudkova <kkeerrll137@gmail.com>
2023-07-18 21:11:11 +0300

    DZ1

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,40 @@
+import json
+import os
 
+from googleapiclient.discovery import build
+
+import isodate
+
+
+# YT_API_KEY скопирован из гугла и вставлен в переменные окружения
+api_key: str = "AIzaSyDzK-EuKi2DdmdlIl9Pl0Xs1HKEVoOk2HI"
+
+# создать специальный объект для работы с API
+youtube = build('youtube', 'v3', developerKey=api_key)
 
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
+        self.title: str = self.get_channel_id()['items'][0]['snippet']['title']
+        self.videoCount: int = self.get_channel_id()['items'][0]['statistics']['videoCount']
+        self.description: str = self.get_channel_id()['items'][0]['snippet']['description']
+        self.url: str = 'https://www.youtube.com/channel/' + self.channel_id
+        self.viewCount: int = self.get_channel_id()['items'][0]['statistics']['viewCount']
 
-    def print_info(self) -> None:
+    def print_json(self, dict_print: dict) -> None:
+        """Выводит словарь в json-подобном формате с отступами"""
+        print(json.dumps(dict_print, indent=2, ensure_ascii=False))
+
+    def get_channel_id(self):
+        """Получить данные о канале по его id"""
+        channel_id = self.channel_id
+        channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        return channel
+
+    def print_info(self, dict_to_print=None) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        channel = self.get_channel_id()
+        self.print_json(channel)
